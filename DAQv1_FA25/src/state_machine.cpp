8b8729c4365431fd3d10f581ba57af727285bf21
Rohith Chikka <rchikka15@gmail.com>
2025-09-27 17:56:27 -0700

    state_machine.cpp

--- DAQv1_FA25/src/state_machine.cpp
+++ DAQv1_FA25/src/state_machine.cpp
@@ -2,27 +2,30 @@
 #include <ADS126X.h>
 #include <SPI.h>
 #include <state_machine.h>
+#include <solenoid_control.h>
+#include <Arduino.h>
 
 #define SENSE_CS_1 40
 #define SENSE_DRDY_1 4 
 #define SENSE_CS_2 42
 #define SENSE_DRDY_2 35
-#define PYRO_CS_1 16
-#define PYRO_CS_2 4
+#define SOLENOID_ACTIVE_HIGH 1  // set to 0 if LOW = open on your hardware
+#define PYRO_CS_1 48
+#define PYRO_CS_2 48
 #define MOSI 5
 #define MISO 41
 #define CLK 13
 
-#define VALVE_FUP 36 // Fuel upstream solenoid open
-#define VALVE_FDP 35 // Fuel downstream solenoid open
-#define VALVE_OUP 34 // LOX upstream solenoid open
-#define VALVE_ODP 33 // LOX downstream solenoid open
-#define FUELVENT 37 //SOL FVP
-#define LOXVENT 38 //SOL OVP
+// #define SOL_FUP 36 // Fuel upstream solenoid open
+// #define SOL_FDP 35 // Fuel downstream solenoid open
+// #define SOL_OUP 34 // LOX upstream solenoid open
+// #define SOL_ODP 33 // LOX downstream solenoid open
+// #define SOL_FVP 37 //SOL FVP
+// #define SOL_OVP 38 //SOL OVP
 #define PRESSURELINE 32
 
-#define FUELMAIN 39 //actuators
-#define LOXMAIN 40 //actuators
+// #define SOL_PVF 39 //actuators
+// #define SOL_PVO 40 //actuators
 
 #define UP_PRESSURE 10
 
@@ -53,8 +56,58 @@ float readPT(int channel) {
   return voltage;
 }
 
-void mosfetCloseAllValves() {
-    for (int i = 0; i < 9; i++) {
-      PYRO_1_MCP->write1(i, 0);
-    }
+
+
+
+void setup() {
+  Serial.begin(115200);
+  static SPIClass& bus = SPI;
+  solenoidsInit(bus, /*SCLK=*/13, /*MISO=*/41, /*MOSI=*/5, /*CS=*/48, /*addr=*/0x00);
+  SPI.begin(CLK, MISO, MOSI, -1);
+  
+
+//   PYRO_1_MCP = new MCP23S17(PYRO_CS_1, 0x00, &SPI);
+//   PYRO_2_MCP = new MCP23S17(PYRO_CS_2, 0x00, &SPI);
+
+//   SENSE_1.begin(SENSE_CS_1);
+//   SENSE_1.startADC1();
+//   SENSE_1.setRate(ADS126X_RATE_1200);
+//   SENSE_1.setReference(ADS126X_REF_NEG_VSS, ADS126X_REF_POS_VDD);
+//   uint8_t power = SENSE_1.readRegister(ADS126X_POWER);
+//   power &= ~(1 << 2);
+//   SENSE_1.writeRegister(ADS126X_POWER);
+
+//   bool status = PYRO_1_MCP->begin();
+//   Serial.println(status ? "Started Pyro 1 GPIO EX" : "Failed to start Pyro 1 GPIO EX");
+//   delay(100);
+//   for (int pin = 0; pin < 7; pin++) {
+//     PYRO_1_MCP->pinMode8(pin, 0x00);
+//     PYRO_1_MCP->write1(pin, 0);
+//   }
+
+//   status = PYRO_2_MCP->begin();
+//   Serial.println(status ? "Started Pyro 2 GPIO EX" : "Failed to start Pyro 2 GPIO EX");
+//   delay(100);
+//   for (int pin = 0; pin < 7; pin++) {
+//     PYRO_2_MCP->pinMode8(pin, 0x00);
+//     PYRO_2_MCP->write1(pin, 0);
+//   }
+
+//   P_threshold_fuel_current = P_threshold_fuel_base;
+//   P_threshold_fuel_down = P_threshold_fuel_base;
+
+//   P_threshold_lox_current = P_threshold_lox_base;
+//   P_threshold_lox_down = P_threshold_lox_base;
+
 }
+
+void loop() {
+  // No Serial parser. Call your control functions from here or other tasks.
+  // Example:
+  openSolenoid(SOL_OUP);
+  delay(1000);
+  closeSolenoid(SOL_OUP);
+  delay(1000);
+  
+}
+
