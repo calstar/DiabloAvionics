75b95b2381daba80b77a334642e820c5ab64fae3
Brent Lee <brent_lee@berkeley.edu>
2025-12-04 22:23:20 -0800

    Migrated working ethernet packet sending code from RTDBoard branch and organized ethernet testing folders

--- Ethernet/EthernetTestSendBasic/src/main.cpp
+++ Ethernet/EthernetTestSendBasic/src/main.cpp
@@ -1,14 +1,15 @@
-#include <WiFi.h>
+#include <Arduino.h>
 #include <ETH.h>
-#include <SPI.h>
-#include <EthernetUdp.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <SPI.h>
+#include <WiFi.h>
 
-#define ETH_CLK_PIN   7
-#define ETH_MISO_PIN  41
-#define ETH_MOSI_PIN  13
-#define ETH_CS_PIN    6
-#define ETH_PHY_ADDR  0
+#define ETH_CLK_PIN 7
+#define ETH_MISO_PIN 41
+#define ETH_MOSI_PIN 13
+#define ETH_CS_PIN 6
+#define ETH_PHY_ADDR 0
 
 IPAddress staticIP(192, 168, 2, 100);
 IPAddress gateway(192, 168, 2, 1);
@@ -18,7 +19,7 @@ IPAddress receiverIP(192, 168, 2, 1);
 const int receiverPort = 5006;
 EthernetUDP udp;
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
 void setup() {
   Serial.begin(115200);
@@ -28,10 +29,10 @@ void setup() {
   // Ethernet.config(staticIP, gateway, subnet, dns);
   Ethernet.init(ETH_CS_PIN);
   delay(1000);
-  
+
   Ethernet.begin(mac, staticIP, dns, gateway, subnet);
   delay(1000);
-  
+
   Serial.println("ESP32 IP: ");
   Serial.println(Ethernet.localIP());
   udp.begin(5005); // Local port for sending
@@ -44,7 +45,6 @@ void loop() {
   int dataLength = dataToSend.length();
   char dataBuffer[dataLength + 1];
   dataToSend.toCharArray(dataBuffer, dataLength + 1);
-
 
   udp.beginPacket(receiverIP, receiverPort);
   udp.write(dataBuffer, dataLength);
